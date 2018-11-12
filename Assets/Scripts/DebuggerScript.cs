722dd8e1c507204c798ecba21938b82e4cfe459f
gta191977649 <episodes27@gmail.com>
2018-11-13 03:42:13 +1100

    add fps display

---
+++ Assets/Scripts/DebuggerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebuggerScript : MonoBehaviour {
+
+	[SerializeField] Text FpsLabel;
+	public float updateTime = 200;
+
+	// Update is called once per frame
+	void Update () {
+		updateTime += (Time.deltaTime - updateTime) * 0.1f;
+		float fps = 1.0f / updateTime;
+		FpsLabel.text = "FPS: "+Mathf.Ceil (fps).ToString ();
+	}
+}
---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Setup : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField] int frameLimit = 60;
+	void Awake () {
+		//限制帧数
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = frameLimit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Application.targetFrameRate != frameLimit)
+              Application.targetFrameRate = frameLimit;
+	}
+}
